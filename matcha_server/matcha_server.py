18baa5a1016d19bd07d704023da302d570acc12f
Hanna Lindgren <han.lind@gmail.com>
2025-05-27 14:39:44 +0200

    matcha_server

--- matcha_server/matcha_server.py
+++ matcha_server/matcha_server.py
@@ -3,16 +3,16 @@
 import os, sys
 
 # Local imports
-import symbolset
+import symbolset, synth
 from matcha_utils import(
     create_path,
     clear_audio,
     find_model,
     get_device,
-    intersperse,
+    #intersperse,
     load_matcha,
     load_vocoder,
-    unbatched_synthesis,
+    #unbatched_synthesis,
     validate_args
 )
 
@@ -45,7 +45,6 @@ def load_config():
         data = json.load(file)
         paths.output = create_path(data['output_path'], create=True)
         paths.models = list(map(create_path, data['model_paths']))
-        #paths.models.insert(0,".")
         force_cpu = data.get('force_cpu', False)
 
         do_clear_audio = True
@@ -54,8 +53,7 @@ def load_config():
 
         if do_clear_audio:
             clear_audio(paths.output)
-            
-        
+                    
         ## read voices in config file
         for voice in data['voices']:
             if 'enabled' in voice and voice['enabled'] == False:
@@ -63,6 +61,8 @@ def load_config():
             name = voice['name']
             model = voice['model']
             vocoder = voice['vocoder']
+
+            ## TODO move setup code to voice.py 
             args = Namespace(
                 name=name,
                 cpu=force_cpu,
@@ -112,9 +112,9 @@ def load_config():
                 symbols = voice['symbols']
             else:
                     raise IOError(f"Voice {name} lacks required 'symbols' definition")
-            synth = Synthesizer(args, device, model, vocoder, denoiser, spk, pzer,  symbolset.Symbols(symbols), config=voice)
+            s = synth.Synthesizer(args, device, model, vocoder, denoiser, spk, pzer,  symbolset.Symbols(symbols), config=voice)
 
-            synths[name] = synth
+            synths[name] = s
             loaded_names.append(name)
     logger.info(f"Loaded voices: {loaded_names}")
 
@@ -122,61 +122,6 @@ def load_config():
 input_types = ['text','phonemes']
 return_types = ['json','wav']
 
-from dataclasses import dataclass
-@dataclass
-class Synthesizer:
-    args: Namespace
-    device: str
-    model: object
-    vocoder: object
-    denoiser: object
-    spk: object
-    phonemizer: object
-    symbolset: object
-    config: object
-
-    def process_input(self, i: int, input: str, input_type: str, device: torch.device):
-        logger.debug(f"process_input [{i}] - Input: {input}")
-        matcha_input = input
-        if input_type == "text":
-            if self.phonemizer is None:
-                msg = f"No phonemizer defined for voice {self.args.name}"
-                logger.error(msg)
-                raise IOError(msg)
-            #text = lowercase(text) # TODO
-            #text = expand_abbreviations(text) # TODO
-            matcha_input = self.phonemizer.phonemize([matcha_input], strip=True, njobs=1)[0]
-            logger.info(f"phonemizer output: {matcha_input}")
-            # Added in some cases espeak is not removing brackets
-            #phonemes = remove_brackets(phonemes) # TODO
-            #phonemes = collapse_whitespace(phonemes) # TODO
-        invalid = []
-        for symbol in matcha_input:
-            if not symbol in self.symbolset.symbol2id:
-                invalid += symbol
-        if len(invalid) > 0:
-            raise KeyError(f"Invalid input symbols: {invalid}")
-        seq = self.symbolset.text_to_sequence(matcha_input)
-        x = torch.tensor(
-            intersperse(seq, 0),
-            dtype=torch.long,
-            device=device,
-        )[None]
-        x_lengths = torch.tensor([x.shape[-1]], dtype=torch.long, device=device)
-        x_phones = self.symbolset.sequence_to_text(x.squeeze(0).tolist())
-        logger.info(f"[{i}] - Processed input: {x_phones[1::2]}")
-        
-        res = {"phonemes": {x_phones[1::2]}, "x_orig": input, "x": x, "x_lengths": x_lengths, "x_phones": x_phones}
-        return res
-    
-    @torch.no_grad()
-    def synthesize(self, params):
-        args = self.args
-        args.speaking_rate = args.speaking_rate * params.speaking_rate
-        logger.debug(f"synthesize params: {params}")
-        return  unbatched_synthesis(args, self.device, self.model, self.vocoder, self.denoiser, params, self.spk, self.symbolset, self.process_input)
-
-        
 synths = {}
 paths = Namespace()
 
