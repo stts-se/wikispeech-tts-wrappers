126ddca723125c38cef1ff591cdab2ffd5aade59
Hanna Lindgren <han.lind@gmail.com>
2026-04-20 21:36:51 +0200

    pausing experiment

--- piper_server/voice.py
+++ piper_server/voice.py
@@ -344,6 +344,8 @@ class Phonemizer:
     def phonemize(self, input, lang=None):
         if input == "":
             return ""
+        elif input == ",":
+            return "__"
         elif input in ["..",".","_","__", "#", "##", ",", ",,", "!", "!!"]: # experiment for pausing
             return input
         elif self.tpe == "deep_phonemizer":
